6c1be07f2e6a62145b7aa72ad2807b55e46f998f
Jørgen Ulrik B. Krag <jubk@magenta-aps.dk>
2014-11-25 15:59:04 +0100

    - DataProvider class for the DoubleHistoryTest.

---
+++ src/main/java/dk/magenta/databroker/jubk/JubkDataProvider.java
@@ -0,0 +1,13 @@
+package dk.magenta.databroker.jubk;
+
+import dk.magenta.databroker.core.DataProvider;
+import dk.magenta.databroker.core.model.DataProviderEntity;
+
+/**
+ * Created by jubk on 11/24/14.
+ */
+public class JubkDataProvider extends DataProvider {
+    public JubkDataProvider(DataProviderEntity dataProviderEntity) {
+        super(dataProviderEntity);
+    }
+}
