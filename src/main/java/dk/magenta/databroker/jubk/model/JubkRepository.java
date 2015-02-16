5add4ddbcbcadc81beb3aff2921ade125c948590
Lars Peter Thomsen <larsp@magenta.dk>
2015-02-16 14:22:37 +0100

    Removed dummy jubk provider

--- src/main/java/dk/magenta/databroker/jubk/model/JubkRepository.java
+++
@@ -1,7 +0,0 @@
-package dk.magenta.databroker.jubk.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface JubkRepository extends JpaRepository<JubkEntity, Long> {
-}
